0f7980e56989d9d785e7badf33968ad9755e3e76
Talles <talles.silva@gmail.com>
2023-11-16 22:14:23 -0300

    Implementing Search and Fetch

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,7 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt="table"
+                        alt="table 1 "
                       />
                       <p>{invoice.name}</p>
                     </div>
