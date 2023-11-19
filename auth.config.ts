c88df117f2ae4dce9961d907bade977f6a52e505
Talles <talles.silva@gmail.com>
2023-11-19 15:51:15 -0300

    Add Screte in Auth.config.js

--- auth.config.ts
+++ auth.config.ts
@@ -4,6 +4,7 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  secret: process.env.SECRET,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
